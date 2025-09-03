4276a82594695d06af6607026e854e80858bde2e
Muhathir . <muhathirmro@gmail.com>
2025-09-03 20:02:40 +0700

    Inisiasi atribut Tugas 2

---
+++ main/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Items(models.Model):
+    CATEGORY_CHOICES = [
+        ('transfer', 'Transfer'),
+        ('update', 'Update'),
+        ('exclusive', 'Exclusive'),
+        ('match', 'Match'),
+        ('rumor', 'Rumor'),
+        ('analysis', 'Analysis'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+
